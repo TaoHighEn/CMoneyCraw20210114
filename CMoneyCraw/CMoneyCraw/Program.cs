79c4263d9ea9ab8ef8037cd4aab5819ea5670ea0
Alan <32477198+TaoHighEn@users.noreply.github.com>
2021-01-14 21:38:04 +0800

    ALNTestCraw

---
+++ CMoneyCraw/CMoneyCraw/Program.cs
@@ -0,0 +1,86 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.OLE.Interop;
+/// <summary>
+/// CMoney爬蟲練習 NBA Player Career Data
+/// 1. Get First Name Char List
+/// 2. Get Player Name And Connection Url
+/// 3. Get Player Career Data
+/// 4. Output Data To CSV.
+/// 2021-01-13
+/// ALN
+/// 直接硬幹
+/// </summary>
+namespace CMoneyCraw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. A~Z 爬不出來偷懶
+            string[] arrays = new string[26];
+            for (int i = 0; i < 26; i++)
+            {
+                arrays[i] = Convert.ToChar(97 + i).ToString();
+            }
+            try
+            {
+                //WebClient webClient = new WebClient();
+                //MemoryStream ms = new MemoryStream(webClient.DownloadData("https://www.basketball-reference.com/players/"));
+                //HtmlDocument doc = new HtmlDocument();
+                // 2. 爬出姓名和連結頁
+                foreach (string ch in arrays)
+                {
+                    List<Player> players = new List<Player>();
+                    HtmlWeb web = new HtmlWeb();
+                    HtmlDocument document = web.Load("https://www.basketball-reference.com/players/" + ch + "/");
+                    HtmlNodeCollection data = document.DocumentNode.SelectNodes($"//*[@id='players']/tbody/tr");
+                    if (data != null)
+                    {
+                        int row = data.Count();
+                        for (int i = 1; i <= row; i++)
+                        {
+                            HtmlNodeCollection nodes = document.DocumentNode.SelectNodes($"//*[@id='players']/tbody/tr[{i}]/th/a");
+                            if (nodes != null)
+                            {
+                                string name = nodes[0].InnerText;
+                                string url = nodes[0].Attributes["href"].Value;
+                                // 3. Get Player Career Data
+                                Console.WriteLine(name + "    " + url);
+                            }
+                        }
+                    }
+                    // 4. Output Char CSV.
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+    }
+    //Model-球員資料
+    public class Player
+    {
+        public string Name { get; set; }
+        public double G { get; set; }
+        public double PTS { get; set; }
+        public double TRB { get; set; }
+        public double AST { get; set; }
+        public double FG { get; set; }
+        public double FG3 { get; set; }
+        public double FT { get; set; }
+        public double eFG { get; set; }
+        public double PER { get; set; }
+        public double WS { get; set; }
+    }
+}
---
+++ test/test.cs
@@ -0,0 +1,44 @@
+using Newtonsoft.Json;
+using System;
+
+namespace TEST
+{
+    public class Library
+    {
+        public static string CallWebApi(string url, object para, string token = null)
+        {
+            try
+            {
+                string parameters = JsonConvert.SerializeObject(para);
+                System.Net.WebRequest req = System.Net.WebRequest.Create(url);
+
+                #region Token
+
+                if (token != null)
+                {
+                    req.PreAuthenticate = true;
+                    req.Headers.Add("Authorization", $"Bearer {token}");
+                }
+
+                #endregion Token
+                req.ContentType = "application/json";
+                req.Method = "POST";
+                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(parameters);
+                req.ContentLength = bytes.Length;
+                System.IO.Stream os = req.GetRequestStream();
+                os.Write(bytes, 0, bytes.Length);
+                os.Close();
+                System.Net.WebResponse resp = req.GetResponse();
+                if (resp == null)
+                    return null;
+                System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+                var result = sr.ReadToEnd().Trim();
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
